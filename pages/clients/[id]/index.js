c04bdba37bc04f138288f82ecbea715e6f46d87c
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-12 23:25:24 +0530

    Navigating programattically & custom 404 page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,26 @@
+import { useRouter } from "next/router";
+
 export default function Clients() {
-    return (
-      <div>
-        <h1> Perticuler Client page</h1>
-      </div>
-         
-    );
+  const router = useRouter();
+
+  function loadRoute() {
+    // router.push('/clients/max/projecta')
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "max",
+        clientprojectid: "project1"
+      }
+    });
+
+    // with replace user will not go to back page
+    // router.replace('/clients/max/projecta')
   }
-  
+
+  return (
+    <div>
+      <h1> Perticuler Client page</h1>
+      <button onClick={loadRoute}>Load project 1</button>
+    </div>
+  );
+}
