9aef39a0510040f0537385b89a3c5db5359ff78a
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-12 14:05:04 +0530

    working with nested paths & routes

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+export default function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio Page</h1>
+      </div>
+         
+    );
+  }
+  
---
+++ pages/portfolio/list.js
@@ -0,0 +1,9 @@
+export default function List() {
+    return (
+      <div>
+        <h1>List Page</h1>
+      </div>
+         
+    );
+  }
+  
