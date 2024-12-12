bffbf6dfb4e5c614a28a8cd5a675ad153e852543
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-12 22:50:34 +0530

    added dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 export default function Clients() {
+
+  const cliets = [{
+    id:'manu' , name : 'Manual'
+  },{
+    id:'max' , name : 'Maximilian'
+  }]
     return (
       <div>
         <h1> Clients page</h1>
+
+        <ul>
+          {
+            cliets.map((x)=>(<li key={x.id}>
+              <Link href={'/clients/'+x.id}> {x.name} </Link>
+            </li>))
+          }
+        
+       
+      </ul>
+
       </div>
          
     );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
  
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+
+
+      <ul>
+        <li>
+          <Link href={'/portfolio'}> Portfolio </Link>
+        </li>
+        <li>
+          <Link href={'/clients'}> Clients </Link>
+        </li>
+      </ul>
     </div>
        
   );
