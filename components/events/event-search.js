c77d7bbd92887da9a1cd4fdbf5e92bbcb154d6b1
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-13 16:06:43 +0530

    added event search component, navigatting filtered events page programmatically

---
+++ components/events/event-search.js
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+import classes from "./events-search.module.css";
+import classes1 from "./event-item.module.css";
+import { useRouter } from "next/router";
+
+export default function EventSearch() {
+  const router = useRouter();
+
+  const monthInputRef = useRef();
+  const yearInputRef = useRef();
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+
+    const selectedMonth = monthInputRef.current.value;
+    router.push(`/events/${selectedYear}/${selectedMonth}`);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlfor="year">Year</label>
+          <select id="year" ref={yearInputRef}>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+
+        <div className={classes.control}>
+          <label htmlfor="month">Month</label>
+          <select id="month" ref={monthInputRef}>
+            <option value="1">january</option>
+            <option value="2">feb</option>
+            <option value="3">mar</option>
+            <option value="4">apr</option>
+            <option value="5">may</option>
+            <option value="6">jun</option>
+            <option value="7">jul</option>
+            <option value="8">aug</option>
+            <option value="9">sept</option>
+            <option value="10">oct</option>
+            <option value="11">nov</option>
+            <option value="12">dec</option>
+          </select>
+        </div>
+      </div>
+
+      <button className={classes1.btn}>Find events</button>
+    </form>
+  );
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,23 @@
+import EventSearch from "@/components/events/event-search";
+import EventList from "@/components/events/evet-list";
+import { getFeaturedEvents } from "@/dummy-data";
+
  
 export default function AllEventsPage() {
-    return (
-      <div>
-        <h1>AllEvents page</h1>
-      </div>
-         
-    );
+
+
+   
+    const events =  getFeaturedEvents();
+   
+     return (
+       <div>
+         <h1>Home page</h1>
+
+        <EventSearch />
+         <EventList items={events}/>
+       </div>
+          
+     );
+
   }
   
