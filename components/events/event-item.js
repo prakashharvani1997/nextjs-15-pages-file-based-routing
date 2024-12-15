dac2a750031bbb982911662bd14a7de78bbdf7a9
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-15 13:20:14 +0530

    optimizing the images with next image component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -4,6 +4,7 @@ import classes from './event-item.module.css'
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
+import Image from "next/image";
  
 export default function EventItem({ item }) {
   const { title, image, date, location, id } = item;
@@ -18,7 +19,7 @@ export default function EventItem({ item }) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={340} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
